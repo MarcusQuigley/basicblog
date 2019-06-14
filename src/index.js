ead4e05aab492ed421f93cdaca16a7cda56260d5
[MarcusQ] <marcusquigley@gmail.com>
2019-06-14 13:44:03 -0400

    spliting out comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import CommentDetail from './CommentDetail';
+import faker from 'faker';
 const App = ()=>{
     return (
-        <div>Here!</div>
+        <div className="ui container comments">
+           <CommentDetail name={faker.name.firstName()} date = "7:00pm" comment="First Commment!" avatar = {faker.image.avatar()}/>
+           <CommentDetail name="Jenn" date = "7:04pm" comment="Like, totally awesome!" avatar = {faker.image.avatar()}/>
+           <CommentDetail name="Joe" date = "8:04pm" comment="Twats" avatar = {faker.image.avatar()}/>
+        </div>
     );
 };
 
